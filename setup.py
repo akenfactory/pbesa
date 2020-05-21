db43d7df9bb66cbbb2093884f0679f9a2eff4b42
fjroldan <fjroldan@itagil.com>
2020-05-21 11:58:05 -0500

    Ajustes de configuracion 3

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from distutils.core import setup
+import setuptools
+
 setup(
   name = 'pbesa',
-  packages = ['pbesa'],
   version = '1.1',
   license='MIT',
   description = 'An artificial intelligence platform for the implementation of multi-agent systems based on python 3 and the BESA model',
   author = 'Enrique Gonzales Guerreo, Cesar Julio Bustacara, Fabian Jose Roldan',
   author_email = 'egonzal@javeriana.edu.co, cbustaca@javeriana.edu.co, fjroldan@akenfactory.com',
   url = 'https://github.com/akenfactory/pbesa.git',
-  download_url = 'https://github.com/akenfactory/pbesa/archive/1.0.tar.gz',
+  download_url = 'https://github.com/akenfactory/pbesa/archive/1.1.tar.gz',
   keywords = ['agent', 'multi-agent', 'system', 'artificial', 'intelligence'],
-  install_requires=[
-          'validators',
-          'beautifulsoup4',
-      ],
+  packages=setuptools.find_packages(),
+  install_requires=[],
   classifiers=[  # Optional
     # How mature is this project? Common values are
     #   3 - Alpha
