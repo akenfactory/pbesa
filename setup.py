59eeed288c82eb659a6dd93d38542615a6d53fb7
fjroldan <fjroldan@akenfactory.com>
2025-04-27 18:45:22 -0500

    Version - 4.0.11

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pbesa',
-    version='4.0.10',
+    version='4.0.11',
     packages=find_packages(),
     install_requires=[
         'pymongo>=4.6.3',
